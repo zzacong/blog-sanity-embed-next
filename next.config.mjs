55d6482f7c1ff82e517bc1c4adb6098902b0a578
Zac Ong <61817066+zzacong@users.noreply.github.com>
2023-03-05 20:06:13 +1100

    app dir

--- next.config.mjs
+++ next.config.mjs
@@ -10,15 +10,9 @@
 const config = {
   reactStrictMode: true,
 
-  /**
-   * If you have the "experimental: { appDir: true }" setting enabled, then you
-   * must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+  experimental: {
+    appDir: true,
   },
 };
+
 export default config;
